032dc88af1b42db7b55afc5cbc0cd536cd680b86
Himanshu Maharshi <himanshu.m_ftc@external.swiggy.in>
2024-10-12 10:29:00 +0530

    used h2 in memory db

--- src/main/java/org/example/wallet/Repositorys/WalletRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.wallet.Repositorys;
-
-import org.example.wallet.Models.Wallet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WalletRepository extends JpaRepository<Wallet, Integer> {
-}
