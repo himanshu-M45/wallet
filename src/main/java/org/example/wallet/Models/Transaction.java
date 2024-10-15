5c374e445d60e09dfaf9403d882ba8d4bb008a0d
Himanshu Maharshi <himanshu.m_ftc@external.swiggy.in>
2024-10-15 22:49:06 +0530

    added timestamp in transactions

--- src/main/java/org/example/wallet/Models/Transaction.java
+++ src/main/java/org/example/wallet/Models/Transaction.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import org.example.wallet.Enums.TransactionType;
 
+import java.util.Date;
+
 @Entity
 @Table(name = "transactions")
 @Getter
@@ -21,10 +23,15 @@ public class Transaction {
     @Column(name = "wallet_id", nullable = false)
     private int walletId;
 
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "timestamp", nullable = false)
+    private Date timestamp;
+
     public Transaction(String message, TransactionType transactionType, int walletId) {
         this.message = message;
         this.transactionType = transactionType;
         this.walletId = walletId;
+        this.timestamp = new Date();
     }
 
     public Transaction() {}
